9e0694c88266e1eb798533a4c46e64811ec2e5a8
harrisch <charlie.harris@wwt.com>
2016-11-13 22:17:01 -0600

    Finishes VFYB and Grants Page

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -7,33 +7,35 @@ import $ from 'jquery';
 import 'waypoints';
 
 $(() => {
-  let topWaypoint = new Waypoint({
-    element: $('#top-splash'),
-    handler: (direction) => {
-      $(window).on('scroll', () => {
-        $('#top-splash > .parallax-image').css('transform', `translateY(${$(window).scrollTop() * -.2}px)`)
-      })
-    },
-    offset: 0
-  })
+  if (window.location.pathname === '/') {
+    let topWaypoint = new Waypoint({
+      element: $('#top-splash'),
+      handler: (direction) => {
+        $(window).on('scroll', () => {
+          $('#top-splash > .parallax-image').css('transform', `translateY(${$(window).scrollTop() * -.2}px)`)
+        })
+      },
+      offset: 0
+    })
 
-  let middleWaypoint = new Waypoint({
-    element: $('#mission-statement'),
-    handler: (direction) => {
-      $(window).on('scroll', () => {
-        $('#mission-statement > .parallax-image').css('transform', `translateY(${($(window).scrollTop() - $('#mission-statement').offset().top) * -.2}px)`)
-      })
-    },
-    offset: '100%'
-  })
+    let middleWaypoint = new Waypoint({
+      element: $('#mission-statement'),
+      handler: (direction) => {
+        $(window).on('scroll', () => {
+          $('#mission-statement > .parallax-image').css('transform', `translateY(${($(window).scrollTop() - $('#mission-statement').offset().top) * -.2}px)`)
+        })
+      },
+      offset: '100%'
+    })
 
-  let bottomWaypoint = new Waypoint({
-    element: $('#vfyb-link'),
-    handler: (direction) => {
-      $(window).on('scroll', () => {
-        $('#vfyb-link > .parallax-image').css('transform', `translateY(${($(window).scrollTop() - $('#vfyb-link').offset().top) * -.2}px)`)
-      })
-    },
-    offset: '100%'
-  })
+    let bottomWaypoint = new Waypoint({
+      element: $('#vfyb-link'),
+      handler: (direction) => {
+        $(window).on('scroll', () => {
+          $('#vfyb-link > .parallax-image').css('transform', `translateY(${($(window).scrollTop() - $('#vfyb-link').offset().top) * -.2}px)`)
+        })
+      },
+      offset: '100%'
+    })
+  }
 });

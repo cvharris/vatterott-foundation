09d1fb3879a445a5a36821743a0653ac24d5c3d4
harrisch <charlie.harris@wwt.com>
2017-02-04 15:28:31 -0600

    Adds key.ring because whatever

--- app/applications/applicationController.js
+++ app/applications/applicationController.js
@@ -17,8 +17,7 @@ const format = require('date-fns/format')
 
 const secret = process.env.SECRET_KEY || require('../../config.js')
 const bucketId = 'b8b4280a0b0d21b73981a333' // grantApplications
-const keyringLocation = process.env.ENV === 'production' ? `${__dirname}/../../..` : `${__dirname}/../..`
-const keyring = storj.KeyRing(keyringLocation, secret);
+const keyring = storj.KeyRing(`${__dirname}/../..`, secret);
 const fileParams = [
   'applicationForm',
   'projectBudget',

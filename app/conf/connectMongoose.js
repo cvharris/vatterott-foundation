fba4bea9741369a070f9cbb339f25918fb9cff7f
cvharris <charris1014@gmail.com>
2020-01-16 10:28:17 -0600

    Swaps project files with Nuxt

--- app/conf/connectMongoose.js
+++ app/conf/connectMongoose.js
@@ -2,7 +2,7 @@
 
 const mongoose = require('mongoose')
 
-const localConnection = 'mongodb://127.0.0.1:27017/vatterottFoundation'
+const localConnection = 'mongodb://heroku_grgdnk4p:e61c35m5jfgp1k3t9gg8i8asfb@ds111589.mlab.com:11589/heroku_grgdnk4p' //'mongodb://127.0.0.1:27017/vatterottFoundation'
 
 mongoose.Promise = global.Promise
 

9d79f1392d4056f7799bdf37186afbb57f3e07d1
Nina Harris <ninaharris121@gmail.com>
2020-03-24 13:33:29 -0500

    Makes admin site look less weird

--- components/AdminComponent.vue
+++ components/AdminComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="container">
+  <div class="container admin-page">
     <div class="align-header">
       <h1 class="page-title">
         Admin
@@ -15,14 +15,15 @@
       <table class="applications-list">
         <thead class="spacing align-body">
           <tr class="grant-application header">
-            <th class="org-col">Organization</th>
-            <th class="contact-col">Contact Info</th>
-            <th class="form-col">Application Form</th>
-            <th class="proj-col">Project Budget</th>
-            <th class="budget-col">Org Budget</th>
-            <th class="irs-col">IRS Letter</th>
-            <th class="update-col">Updated At</th>
-            <!-- <th class="action-col"></th> -->
+            <th class="column">Organization</th>
+            <th class="column">Contact Person</th>
+            <th class="colum">Phone Number</th>
+            <th class="column">Application Form</th>
+            <th class="column">Project Budget</th>
+            <th class="column">Org Budget</th>
+            <th class="column">IRS Letter</th>
+            <th class="column">Updated At</th>
+            <th class="column">Delete</th>
           </tr>
         </thead>
         <tbody>
@@ -37,36 +38,32 @@
             :key="appl.id"
             class="grant-application"
           >
-            <td class="org-col">{{ appl.company }}</td>
-            <td class="contact-col">
-              <span v-if="!!appl.contactName">{{ appl.contactName }}</span
-              ><span v-if="!!appl.contactName && !!appl.contactPhone"> - </span
-              ><span v-if="!!appl.contactPhone">{{ appl.contactPhone }}</span>
-            </td>
-            <td class="form-col">
+            <td class="column">{{ appl.company }}</td>
+            <td class="column">{{ appl.contactName }}</td>
+            <!-- <div v-if="!!appl.contactName && !!appl.contactPhone"></div> -->
+            <td class="column">{{ appl.contactPhone }}</td>
+            <td class="column">
               <span v-show="!appl.applicationForm" class="no-link"></span>
               <div v-show="appl.applicationForm" class="file-actions">
-                {{ getFileNameFromPath(appl.applicationForm) }}
-                <span
+                <div
                   @click="downloadFile(appl.applicationForm)"
                   class="file-button download"
                 >
                   <i class="fa fa-download"></i>
-                </span>
-                <span
+                </div>
+                <div
                   @click="
                     deleteFile(appl.id, appl.applicationForm, 'applicationForm')
                   "
                   class="file-button trash secondary"
                 >
                   <i class="fa fa-trash"></i>
-                </span>
+                </div>
               </div>
             </td>
-            <td class="proj-col">
+            <td class="column">
               <span v-show="!appl.projectBudget" class="no-link"></span>
               <div v-show="appl.projectBudget" class="file-actions">
-                {{ getFileNameFromPath(appl.projectBudget) }}
                 <span
                   @click="downloadFile(appl.projectBudget)"
                   class="file-button download"
@@ -83,10 +80,9 @@
                 </span>
               </div>
             </td>
-            <td class="budget-col">
+            <td class="column">
               <span v-show="!appl.orgBudget" class="no-link"></span>
               <div v-show="appl.orgBudget" class="file-actions">
-                {{ getFileNameFromPath(appl.orgBudget) }}
                 <span
                   @click="downloadFile(appl.orgBudget)"
                   class="file-button download"
@@ -101,10 +97,9 @@
                 </span>
               </div>
             </td>
-            <td class="irs-col">
+            <td class="column">
               <span v-show="!appl.irsLetter" class="no-link"></span>
               <div v-show="appl.irsLetter" class="file-actions">
-                {{ getFileNameFromPath(appl.irsLetter) }}
                 <span
                   @click="downloadFile(appl.irsLetter)"
                   class="file-button download"
@@ -119,16 +114,18 @@
                 </span>
               </div>
             </td>
-            <td class="update-col">
+            <td class="column">
               {{ appl.updatedAt }}
             </td>
-            <td class="action-col">
-              <span
-                @click="deleteApplication(appl.id)"
-                class="file-button trash"
-              >
-                <i class="fa fa-trash"></i>
-              </span>
+            <td class="column">
+              <div class="file-actions">
+                <span
+                  @click="deleteApplication(appl.id)"
+                  class="file-button trash secondary"
+                >
+                  <i class="fa fa-trash"></i>
+                </span>
+              </div>
             </td>
           </tr>
         </tbody>
@@ -236,5 +233,11 @@ export default {
   display: flex;
   justify-content: space-between;
   align-items: center;
+}
+
+.column {
+  padding-right: 5px;
+  padding-left: 5px;
+  text-align: center;
 }
 </style>
